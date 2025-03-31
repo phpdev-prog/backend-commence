233c40c592977337dcb33a7143d35cb877382c95
phpdev-prog <phpdeveloper2k23@gmail.com>
2025-03-31 16:06:49 +0530

    Node js Start....

---
+++ index.js
@@ -0,0 +1,52 @@
+const colors = require('colors')
+
+console.log("Hello world!".bgRed);
+
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
+
+// const math = require('./math')
+
+// console.log(math.add(2,5))
+// console.log(math.subtract(2,5))
+
+
+// examples
+
+// eg1. 
+
+/*
+****
+****
+****
+****
+
+eg2.
+
+R
+RA
+RAS
+RASH
+RASHI
+
+eg3.
+
+* * * *
+*     *
+*     *
+*     *
+* * * *
+
+
+eg4
+
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+
+ */
---
+++ math.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+
+// module.exports = add
+
+// module.exports = subtract
+
+module.exports = {
+    add,
+    subtract
+}
